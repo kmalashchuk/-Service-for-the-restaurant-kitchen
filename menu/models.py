e6a3f22f0562aabccda4c98dfafa0d87fd8c157f
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-05-20 22:13:18 +0300

    create application models, added app to settings.py, add db.sqlite3

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+
+
+
+class Cookie(models.Model):
+    name = models.CharField(max_length=100)
