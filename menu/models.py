c720a23f3858746cbe6234a03d4d878f5d445c74
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-05-20 22:43:35 +0300

    appdate application models, added abstractuser to settings.py, make miggrations

--- menu/models.py
+++ menu/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
 class DishType(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
@@ -9,13 +11,30 @@ class DishType(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Dish(models.Model):
-    name = models.CharField(max_length=100)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    ingredients = models.ManyToManyField(Ingredient)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField("Cook", related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    def __str__(self):
+        return self.name
 
 
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+    specialization = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Specialization of the cook",
+    )
 
-class Cookie(models.Model):
-    name = models.CharField(max_length=100)
+
+   def __str__(self):
+       return f"{self.username} ({self.first_name} {self.last_name})"
